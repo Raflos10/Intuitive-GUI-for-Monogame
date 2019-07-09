6127e3e725b4556296ab044167e4990f4854f96b
Raflos <raflos5661@gmail.com>
2019-07-08 20:31:37 -0700

    Added build random grid option to Demo

--- Demo/Demo Menu/Menu1.cs
+++ Demo/Demo Menu/Menu1.cs
@@ -21,12 +21,22 @@ namespace Demo.Demo_Menu
     {
         public Texture2D Background { get; set; }
 
-        public Menu1(Texture2D texture, Texture2D buttonTexture1, Texture2D buttonTexture2, Texture2D imageTexture, BitmapFont bitmapFont)
+		private Texture2D buttonTexture1, buttonTexture2, imageTexture1, imageTexture2;
+		private BitmapFont bitmapFont;
+
+        public Menu1(Texture2D texture, Texture2D buttonTexture1, Texture2D buttonTexture2, Texture2D imageTexture1, 
+			Texture2D imageTexture2, BitmapFont bitmapFont)
         {
             this.Background = texture;
             this.Width = texture.Width;
             this.Height = texture.Height;
             Origin = new Vector2(Width * .5f, Height * .5f);
+
+			this.buttonTexture1 = buttonTexture1;
+			this.buttonTexture2 = buttonTexture2;
+			this.imageTexture1 = imageTexture1;
+			this.imageTexture2 = imageTexture2;
+			this.bitmapFont = bitmapFont;
 
             Grid subGrid = new Grid();
 
@@ -57,19 +67,19 @@ namespace Demo.Demo_Menu
             mainGrid.AddChild(new Menu1Button(buttonTexture1, OnButtonPress, new ButtonArgs("0, 0")), 0, 0);
             mainGrid.AddChild(new Menu1Button(buttonTexture1, OnButtonPress, new ButtonArgs("1, 0")), 1, 0);
             mainGrid.AddChild(new Menu1Button(buttonTexture1, OnButtonPress, new ButtonArgs("2, 0")), 2, 0);
-            mainGrid.AddChild(new Image(imageTexture), 3, 0);
+            mainGrid.AddChild(new Image(imageTexture1), 3, 0);
             mainGrid.AddChild(new Menu1Button(buttonTexture1, OnButtonPress, new ButtonArgs("4, 0")), 4, 0);
 
             mainGrid.AddChild(new Menu1Button(buttonTexture1, OnButtonPress, new ButtonArgs("0, 1")), 0, 1);
             mainGrid.AddChild(subGrid, 1, 1);
             mainGrid.AddChild(new Menu1Button(buttonTexture1, OnButtonPress, new ButtonArgs("2, 1")), 2, 1);
             mainGrid.AddChild(new Menu1TextButton(bitmapFont, "Button", OnButtonPress, new ButtonArgs("3, 1")), 3, 1);
-            mainGrid.AddChild(new Image(imageTexture), 4, 1);
+            mainGrid.AddChild(new Image(imageTexture1), 4, 1);
 
-            mainGrid.AddChild(new Image(imageTexture), 0, 2);
+            mainGrid.AddChild(new Image(imageTexture1), 0, 2);
             mainGrid.AddChild(new Menu1Button(buttonTexture1, OnButtonPress, new ButtonArgs("1, 2")), 1, 2);
             mainGrid.AddChild(new Menu1Button(buttonTexture1, OnButtonPress, new ButtonArgs("2, 2")), 2, 2);
-            mainGrid.AddChild(new Image(imageTexture), 3, 2);
+            mainGrid.AddChild(new Image(imageTexture1), 3, 2);
             mainGrid.AddChild(new Menu1Button(buttonTexture1, OnButtonPress, new ButtonArgs("4, 2")), 4, 2);
 
             mainGrid.AddChild(new Menu1Button(buttonTexture1, OnButtonPress, new ButtonArgs("0, 3")), 0, 3);
@@ -80,6 +90,84 @@ namespace Demo.Demo_Menu
 
             Item = mainGrid;
         }
+
+		public void RandomGrid()
+		{
+			Random random = new Random();
+
+			Grid subGrid1 = new Grid();
+			subGrid1.AddColumnDefinition(ColumnDefinition.Auto);
+			subGrid1.AddColumnDefinition(ColumnDefinition.Auto);
+			subGrid1.AddRowDefinition(RowDefinition.Auto);
+			subGrid1.AddRowDefinition(RowDefinition.Auto);
+
+			Point subGrid1Location = randomLocation();
+
+			for (int x = 0; x < 2; x++)
+				for (int y = 0; y < 2; y++)
+					subGrid1.AddChild(randomElement(x,y,true), x, y);
+
+					Grid subGrid2 = new Grid();
+			subGrid2.AddColumnDefinition(ColumnDefinition.Auto);
+			subGrid2.AddColumnDefinition(ColumnDefinition.Auto);
+			subGrid2.AddRowDefinition(RowDefinition.Auto);
+			subGrid2.AddRowDefinition(RowDefinition.Auto);
+
+			Point subGrid2Location = randomLocation();
+
+			for (int x = 0; x < 2; x++)
+				for (int y = 0; y < 2; y++)
+					subGrid2.AddChild(randomElement(x, y, true), x, y);
+
+			while (subGrid1Location == subGrid2Location)
+				subGrid2Location = randomLocation();
+
+			Grid mainGrid = new Grid(new Margin(100, 0, 100, 0));
+			mainGrid.PersistantHighlight = true;
+
+			mainGrid.AddColumnDefinition(ColumnDefinition.Auto);
+			mainGrid.AddColumnDefinition(ColumnDefinition.Auto);
+			mainGrid.AddColumnDefinition(ColumnDefinition.Auto);
+			mainGrid.AddColumnDefinition(ColumnDefinition.Auto);
+			mainGrid.AddColumnDefinition(ColumnDefinition.Auto);
+			mainGrid.AddColumnDefinition(ColumnDefinition.Fill);
+			mainGrid.AddRowDefinition(RowDefinition.Auto);
+			mainGrid.AddRowDefinition(RowDefinition.Auto);
+			mainGrid.AddRowDefinition(RowDefinition.Auto);
+			mainGrid.AddRowDefinition(RowDefinition.Auto);
+			mainGrid.AddRowDefinition(RowDefinition.Fill);
+
+			for (int x = 0; x < 5; x++)
+				for (int y = 0; y < 4; y++)
+				{
+					if (new Point(x, y) == subGrid1Location)
+						mainGrid.AddChild(subGrid1, x, y);
+					else if (new Point(x, y) == subGrid2Location)
+						mainGrid.AddChild(subGrid2, x, y);
+					else
+					mainGrid.AddChild(randomElement(x, y, false), x, y);
+				}
+
+			Item = mainGrid;
+
+			Point randomLocation()
+			{
+				return new Point(random.Next(0, 5), random.Next(0, 4));
+			}
+
+			UIElement randomElement(int x, int y, bool small)
+			{
+				int decider = random.Next(0, 3);
+				switch(decider){
+					case 0:
+						return new Menu1Button(small?buttonTexture2:buttonTexture1, OnButtonPress, new ButtonArgs((x + ", " + y).ToString()));
+					case 1:
+						return new Menu1TextButton(bitmapFont, "Button", OnButtonPress, new ButtonArgs((x + ", " + y).ToString()));
+					default:
+						return new Image(small?imageTexture2:imageTexture1);
+				}
+			}
+		}
 
         void OnButtonPress(object sender, EventArgs args)
         {
--- Demo/Game1.cs
+++ Demo/Game1.cs
@@ -85,7 +85,8 @@ namespace Demo
             Components.Add(menuSystem);
 
             Menu1 = new Demo_Menu.Menu1(Content.Load<Texture2D>("test_menu"), Content.Load<Texture2D>("test_button"),
-                Content.Load<Texture2D>("test_button_s"), Content.Load<Texture2D>("test_image_s"), Content.Load<BitmapFont>("Font/Calibri48"));
+                Content.Load<Texture2D>("test_button_s"), Content.Load<Texture2D>("test_image"),
+				Content.Load<Texture2D>("test_image_s"), Content.Load<BitmapFont>("Font/Calibri48"));
             menuSystem.OpenMenu(Menu1);
             Menu1.Position = new Vector2(VirtualWidth * .5f, VirtualHeight * .5f);
             //Menu1.Position = new Vector2((VirtualWidth - Menu1.Width) * .5f, 100);
@@ -146,6 +147,9 @@ namespace Demo
                     selectable.PersistantHighlight = !selectable.PersistantHighlight;
                     Debug.WriteLine("Persistant Highlighting " + (selectable.PersistantHighlight ? "On" : "Off"));
                 }
+
+			if (Input.IsKeyTriggered(Keys.R))
+				Menu1.RandomGrid();
 
             base.Update(gameTime);
         }
