c6db09fdc34c83c15c984d8b42aac680e4c84dc2
Raflos <raflos5661@gmail.com>
2020-03-02 16:28:57 -0800

    add uicontainer object

--- Intuitive GUI for Monogame/Items/Grid.cs
+++ Intuitive GUI for Monogame/Items/Grid.cs
@@ -12,7 +12,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Intuitive_GUI_for_Monogame.Items
 {
-	public class Grid : Selectable
+	public class Grid : UIContainer
 	{
 		#region Read-Only Lists
 
---
+++ Intuitive GUI for Monogame/Items/UIContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Intuitive_GUI_for_Monogame.Items
+{
+	public abstract class UIContainer : Selectable
+	{
+
+	}
+}
